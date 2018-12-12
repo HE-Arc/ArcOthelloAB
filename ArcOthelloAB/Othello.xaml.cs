467a8771d1073dee8304e48fa30827142a3830ba
kim.biloni <kim.biloni@he-arc.ch>
2018-12-12 20:33:00 +0100

    First version of Othello.xaml

--- ArcOthelloAB/Othello.xaml.cs
+++ ArcOthelloAB/Othello.xaml.cs
@@ -32,5 +32,10 @@ namespace ArcOthelloAB
             parent.Show();
             this.Close();
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            parent.Show();
+        }
     }
 }
