6804c13a7167e9de7935763c6e52d743d31bb8a2
kim.biloni <kim.biloni@he-arc.ch>
2018-12-20 09:41:41 +0100

    create OthelloBoard

---
+++ ArcOthelloAB/OthelloBoard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IPlayable;
+
+namespace ArcOthelloAB
+{
+    class OthelloBoard : IPlayable.IPlayable
+    {
+
+        ///
+        /// IPlayable functions
+        ///
+        
+        public int GetBlackScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[,] GetBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetWhiteScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPlayable(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayMove(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
