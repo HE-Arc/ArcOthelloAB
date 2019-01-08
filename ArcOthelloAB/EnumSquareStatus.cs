3754f810a42862f26873b481cc7b8c5cf1e2db1d
Arzul Paul <paul.arzul@he-arc.ch>
2019-01-08 18:14:04 +0100

    reorganisd code + added color to button

---
+++ ArcOthelloAB/EnumSquareStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcOthelloAB
+{
+    public enum SquareStatus { NoPawn = 0, BlackPawn = 1, WhitePawn = 2 };
+}
--- ArcOthelloAB/Othello.xaml.cs
+++ ArcOthelloAB/Othello.xaml.cs
@@ -24,24 +24,13 @@ namespace ArcOthelloAB
         // UI properties
         private Window parent;
         private UIElement[,] buttons;
-
-        public static readonly DependencyProperty IsAvailableProperty =
-            DependencyProperty.Register(
-            "isAvailable", typeof(Boolean),
-            typeof(Othello)
-            );
+        private ButtonHandler buttonHandler;
 
         // Game Properties
         private static int TOTAL_ROW = 7;
         private static int TOTAL_COLLUMN = 9;
 
-        enum SquareStatus { NoPawn=0, BlackPawn=1, WhitePawn=2 };
-        // Uses the SquareStatus enum for the state
-        public static readonly DependencyProperty CurrentStatus =
-            DependencyProperty.Register(
-            "currentStatus", typeof(SquareStatus),
-            typeof(Othello)
-            );
+        public SquareStatus status { get; set; }
 
         private bool AIPlayer;
 
@@ -69,7 +58,8 @@ namespace ArcOthelloAB
             this.parent = parent;
             InitializeComponent();
 
-            setupButtons();
+            buttons = new UIElement[TOTAL_COLLUMN, TOTAL_ROW];
+            buttonHandler = new ButtonHandler(this.GameGrid, buttons);
 
             context = new TimeHandler { TimePlayedWhite = 10 , TimePlayedBlack = 5};
 
@@ -101,68 +91,6 @@ namespace ArcOthelloAB
                 MessageBox.Show(this, ex.Message);
                 BtnBack_Click(null, null);
             }
-        }
-
-        private void setupButtons()
-        {
-            buttons = new UIElement[TOTAL_COLLUMN, TOTAL_ROW];
-
-            for (int i = 0; i < TOTAL_COLLUMN; i++)
-            {
-                for (int j = 0; j < TOTAL_ROW; j++)
-                {
-                    Button btn = new Button();
-                    btn.Content = i + " , " + j;
-                    int btnX = i;
-                    int btnY = j;
-                    btn.Click += (sender, e) =>
-                    {
-                        ButtonAction1(btnX, btnY); // temporary
-                    };
-                    this.GameGrid.Children.Add(btn); // add button as children to gamegrid
-                    Grid.SetRow(btn, j); // search the btn contained in a grid and setup a row position
-                    Grid.SetColumn(btn, i); // setup a column
-
-                    buttons[btnX, btnY] = GetButton(btnX, btnY);
-                }
-            }
-
-            // initiate property of each button
-            foreach (UIElement child in this.GameGrid.Children)
-            {
-                child.SetValue(IsAvailableProperty, false);
-                child.SetValue(CurrentStatus, SquareStatus.NoPawn);
-            }
-
-            // setup initial board
-            buttons[3, 3].SetValue(CurrentStatus, SquareStatus.WhitePawn);
-            buttons[4, 3].SetValue(CurrentStatus, SquareStatus.BlackPawn);
-            buttons[3, 4].SetValue(CurrentStatus, SquareStatus.BlackPawn);
-            buttons[4, 4].SetValue(CurrentStatus, SquareStatus.WhitePawn);
-
-            for (int i = 0; i < TOTAL_COLLUMN; i++)
-            {
-                for (int j = 0; j < TOTAL_ROW; j++)
-                {
-                    if((SquareStatus)buttons[i,j].GetValue(CurrentStatus) == SquareStatus.NoPawn)
-                        UpdateButtonAvailability(i, j);
-                }
-            }
-        }
-        /*
-         * get an UIElement of the board grid according to it's row and column
-         */
-        private UIElement GetButton(int column, int row)
-        {
-            Grid grid = this.GameGrid;
-            foreach (UIElement child in grid.Children)
-            {
-                if (Grid.GetRow(child) == row && Grid.GetColumn(child) == column)
-                {
-                    return child;
-                }
-            }
-            return null;
         }
 
         private void BtnSettings_Click(object sender, RoutedEventArgs e)
@@ -241,93 +169,6 @@ namespace ArcOthelloAB
             parent.Show();
         }
 
-
-        /*
-         * temporary
-         */
-        protected void ButtonAction1(int x, int y)
-        {
-            UIElement currentutton = buttons[x, y];
-            MessageBox.Show(this, "Hello, button : " + x + " , " + y + "  I am : " + currentutton.GetValue(CurrentStatus));
-            MessageBox.Show(this, " can be played? : " + currentutton.GetValue(IsAvailableProperty));
-        }
-
-        /*
-         * Coordonate of square to check ar given as argument
-         * 
-         * Will check in every direction to see if this square is playable
-         * Will then update his status according to the result of the search
-         */
-        private void UpdateButtonAvailability (int x, int y)
-        {
-            int[,] directionToCheck = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
-
-            bool isPlayable = false;
-
-            for (int i = 0; i<8; i++)
-            {
-                int dirX = directionToCheck[i, 0];
-                int dirY = directionToCheck[i, 1];
-                if (CheckOtherPawnFromDirection(dirX, dirY, x, y, SquareStatus.BlackPawn))
-                    isPlayable = true;
-            }
-            if (isPlayable)
-                buttons[x,y].SetValue(IsAvailableProperty, true);
-            else
-                buttons[x, y].SetValue(IsAvailableProperty, false);
-        }
-
-        /**
-         * Will check in a given direction if the a pawn can be placed on the current empty square
-         * 
-         * The direction is given by a movement in x and y: dirX and dirY argument
-         * These value should be {-1,0,1} with at least one of the two not set at 0
-         * 
-         * the position of empty square is given by x and y argument
-         */
-        private bool CheckOtherPawnFromDirection (int dirX, int dirY, int x, int y, SquareStatus currentStatus)
-        {
-            if (dirX == 0 && dirY == 0)
-                return false;
-            x += dirX; // verification will advance in the given direction
-            y += dirY;
-
-            // if the next square is out of the grid
-            if (x < 0 || x >= TOTAL_COLLUMN || y < 0 || y >= TOTAL_ROW)
-                return false;
-            UIElement currentButton = buttons[x, y];
-
-            // get the color of the current ennemy
-            SquareStatus opponentStatus;
-            if (currentStatus == SquareStatus.BlackPawn)
-                opponentStatus = SquareStatus.WhitePawn;
-            else
-                opponentStatus = SquareStatus.BlackPawn;
-
-            SquareStatus nextPawnStatus = (SquareStatus)currentButton.GetValue(CurrentStatus);
-
-            bool hasAtLeastOneEnnemy = false;
-
-            // as long as there is only ennemy pawn alligned
-            while (nextPawnStatus == opponentStatus)
-            {
-                hasAtLeastOneEnnemy = true;
-                x += dirX;
-                y += dirY;
-
-                if (x < 0 || x >= TOTAL_COLLUMN || y < 0 || y >= TOTAL_ROW)
-                    return false;
-                currentButton = buttons[x, y];
-                nextPawnStatus = (SquareStatus)currentButton.GetValue(CurrentStatus);
-            }
-
-            // when a checked pawn isn't ennemy pawn, if it one of his pawn, he can place his pawn in the checked square
-            if (nextPawnStatus == currentStatus && hasAtLeastOneEnnemy)
-                return true;
-            else
-                return false;
-        }
-
         /// <summary>
         /// Load a game from a file
         /// <param name=path>path of the game file</param>
@@ -401,13 +242,7 @@ namespace ArcOthelloAB
         /// </summary>
         private int GetScore(SquareStatus pawnStatus)
         {
-            int score = 0;
-            foreach (var button in buttons)
-            {
-                if ((SquareStatus)button.GetValue(CurrentStatus) == pawnStatus)
-                    score++;
-            }
-            return score;
+            return buttonHandler.GetScore(pawnStatus);
         }
 
         ///
@@ -422,7 +257,7 @@ namespace ArcOthelloAB
             {
                 for (int j = 0; j < TOTAL_ROW; j++)
                 {
-                    switch ((SquareStatus)buttons[i, j].GetValue(CurrentStatus))
+                    switch (buttonHandler.getStatus(i,j))
                     {
                         case SquareStatus.NoPawn:
                             intBoard[i, j] = 0;
