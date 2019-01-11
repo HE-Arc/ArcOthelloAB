d2a169995497c99d3b57917c1182ae349ed82486
Kim Biloni <kim.biloni@he-arc.ch>
2019-01-11 10:21:20 +0100

    removing unused import

--- ArcOthelloAB/ButtonHandler.cs
+++ ArcOthelloAB/ButtonHandler.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.IO;
-using System.Reflection;
-using System.Windows.Media;
-using System.Drawing;
-using System.Web;
-using System.Windows.Media.Imaging;
-using System.Windows.Resources;
 
 namespace ArcOthelloAB
 {
