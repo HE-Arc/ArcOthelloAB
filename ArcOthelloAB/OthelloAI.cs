34170b1d163ad544b3bf4b97c2789953a849eae0
kim.biloni <kim.biloni@he-arc.ch>
2019-02-15 16:50:45 +0100

    WIP implements alphaBeta

--- ArcOthelloAB/OthelloAI.cs
+++ ArcOthelloAB/OthelloAI.cs
@@ -35,8 +35,11 @@ namespace ArcOthelloAB
         /// <returns>Best move to play</returns>
         public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
         {
-            //TODO
-            return null;
+            MoveNode root = new MoveNode(game);
+            bool minOrMax = whiteTurn;  // TODO need to see when we must min or max
+            int parentValue = 0; // TODO 0 or 1 for begin search value
+            Tuple<int, Tuple<int, int>> bestMove = AlphaBeta(root, level, minOrMax, parentValue);
+            return bestMove.Item2;
         }
 
         /// <summary>
@@ -49,6 +52,24 @@ namespace ArcOthelloAB
         /// <returns></returns>
         private Tuple<int, Tuple<int, int>> AlphaBeta(MoveNode root, int depth, bool minOrMax, int parentValue)
         {
+            if (depth == 0 || root.Final())
+                return new Tuple<int, Tuple<int, int>>(root.Eval(), null);
+            int optVal = 0;
+            if(minOrMax) // if true -> maximaze
+            {
+                optVal = Int32.MinValue;
+            }
+            else    // else minimize
+            {
+                optVal = Int32.MaxValue;
+            }
+            Tuple<int, int> moveOperator = null;
+
+            foreach(var move in root.GetPossibleOperators())
+            {
+                // search th best move
+            }
+
             return null;
         }
 
@@ -83,6 +104,15 @@ namespace ArcOthelloAB
         /// </summary>
         private class MoveNode
         {
+            /// <summary>
+            /// Construct a move node using a game board
+            /// </summary>
+            /// <param name="game"></param>
+            public MoveNode(int[,] game)
+            {
+                // TODO
+            }
+
             /// <summary>
             /// Constructor of MoveNode
             /// </summary>
