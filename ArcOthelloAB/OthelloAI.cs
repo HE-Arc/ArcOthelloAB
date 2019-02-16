82ae49830cb28472cc6a6e3235edab30bc79e5ac
Arzul Paul <paul.arzul@he-arc.ch>
2019-02-16 15:16:15 +0100

    added possible operator to MoveNode

--- ArcOthelloAB/OthelloAI.cs
+++ ArcOthelloAB/OthelloAI.cs
@@ -172,8 +172,23 @@ namespace ArcOthelloAB
             /// <returns></returns>
             public List<Tuple<int, int>> GetPossibleOperators()
             {
-                // TODO, evaluate all square without pawn to see if they're playable, then add to a list
-                return null;
+                List<Tuple<int, int>> possibleOperators = new List<Tuple<int, int>>();
+
+                int collumnLenght = game.GetLength(0);
+                int rowLenght = game.GetLength(1);
+                for (int i = 0; i < collumnLenght; i++)
+                {
+                    for (int j = 0; j < rowLenght; j++)
+                    {
+                        if(game[i,j] == 0)
+                        {
+                            if (checkPlayableSquare())
+                                possibleOperators.Add(new Tuple<int, int>(x, y));
+                        }
+                    }
+                }
+
+                return possibleOperators;
             }
 
             /// <summary>
@@ -194,6 +209,93 @@ namespace ArcOthelloAB
                     child.isWhiteTurn = this.isWhiteTurn;  // after switching turn, if there's still no move available, the game ended, function Final() will do this verification
 
                 return child;
+            }
+
+
+            //////////////////////
+            //      PRIVATE     //
+            //////////////////////
+
+            /// <summary>
+            /// Check if a given square is playable
+            /// 
+            /// return true if it is
+            /// </summary>
+            /// <param name="x">x position of the current square to check</param>
+            /// <param name="y">y position of the current square to check</param>
+            /// <returns></returns>
+            private bool checkPlayableSquare(int x, int y)
+            {
+                int[,] directionToCheck = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
+
+                bool isPlayable = false;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    int dirX = directionToCheck[i, 0];
+                    int dirY = directionToCheck[i, 1];
+                    if (CheckOtherPawnFromDirection(dirX, dirY, x, y))
+                        isPlayable = true;
+                }
+                return isPlayable;
+            }
+
+            /// <summary>
+            /// Will check in a given direction if the a pawn can be placed on the current empty square
+            /// </summary>
+            /// <param name=dirX> direction ot move for each step in x coordonate</param>
+            /// <param name=dirY> direction ot move for each step in y coordonate</param>
+            /// <param name=x> x coordonate of clicked button</param>
+            /// <param name=y> y coordonate of clicked button</param>
+            /// <param name=currentStatus> status of the current player</param>
+            private bool CheckOtherPawnFromDirection(int dirX, int dirY, int x, int y)
+            {
+                if (dirX == 0 && dirY == 0)
+                    return false;
+                x += dirX; // verification will advance in the given direction
+                y += dirY;
+
+                // if the next square is out of the grid
+                if (x < 0 || x >= game.GetLength(0) || y < 0 || y >= game.GetLength(1))
+                    return false;
+
+                int nextSquareValue = board[x, y];
+
+                int playerValue;
+                int opponentValue;
+                if (isWhiteTurn)
+                {
+                    playerValue = 1;
+                    opponentValue = 2;
+                }
+                else
+                {
+                    playerValue = 2;
+                    opponentValue = 1;
+                }
+
+                SquareStatus nextPawnStatus = (SquareStatus)currentButton.GetValue(CurrentStatus);
+
+                bool hasAtLeastOneEnnemy = false; // if there isn't a single ennemy and the search stoped then it must not return false
+
+                // as long as there is only ennemy pawn alligned
+                while (nextSquareValue == opponentValue)
+                {
+                    hasAtLeastOneEnnemy = true;
+                    x += dirX;
+                    y += dirY;
+
+                    if (x < 0 || x >= TOTAL_COLLUMN || y < 0 || y >= TOTAL_ROW)
+                        return false;
+
+                    nextSquareValue = board[x, y];
+                }
+
+                // when a checked pawn isn't ennemy pawn, if it one of his pawn, he can place his pawn in the checked square
+                if (nextSquareValue == playerValue && hasAtLeastOneEnnemy)
+                    return true;
+                else
+                    return false;
             }
         }
     }
